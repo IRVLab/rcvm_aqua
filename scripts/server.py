ee04b64dc7dbc9d74819ecd5d5fcea8629ed552a
Michael Fulton <fulto081@umn.edu>
2018-10-03 11:41:46 -0500

    rcvm 0.5.0 refactor

--- scripts/server.py
+++ scripts/server.py
@@ -5,187 +5,133 @@ from aquacore.msg import Command
 from rcvm.srv import *
 
 
-# A head nodding motion, indicating yes.
-def handle_nod(req):
-    # Check defined cycles and set to default if none, return false if invalid.
-    cycles = req.cycles
-    if cycles == 0 or cycles == None:
-	    cycles = 2
-    elif cycles > 5:
-        return NodResponse(False)
-
+# A head affirmativeding motion, indicating affirmative.
+def handle_affirmative(req):
     # Check defined emphasis and set to default if none, return false if invalid.
     emphasis = req.emphasis
     if emphasis == 0 or emphasis == None:
         emphasis == 0.5
     elif emphasis < 0.0 or emphasis > 1.0:
-        return NodResponse(False)
+        return AffirmativeResponse(False)
  
-    return NodResponse(True)
+    return AffirmativeResponse(True)
 
 
 # A head shaking motion, indicating no.
-def handle_shake(req):
-    # Check defined cycles and set to default if none, return false if invalid.
-    cycles = req.cycles
-    if cycles == 0 or cycles == None:
-	    cycles = 2
-    elif cycles > 5:
-        return NodResponse(False)
-
+def handle_negative(req):
     # Check defined emphasis and set to default if none, return false if invalid.
     emphasis = req.emphasis
     if emphasis == 0 or emphasis == None:
         emphasis == 0.5
     elif emphasis < 0.0 or emphasis > 1.0:
-        return NodResponse(False)
+        return NegativeResponse(False)
 
-    return ShakeResponse(True)
+    return NegativeResponse(True)
 
 
-# A head bobble motion indicating a maybe.
-def handle_bobble(req):
-    # Check defined cycles and set to default if none, return false if invalid.
-    cycles = req.cycles
-    if cycles == 0 or cycles == None:
-	    cycles = 2
-    elif cycles > 5:
-        return NodResponse(False)
-
+# A head possible motion indicating a maybe.
+def handle_possible(req):
     # Check defined emphasis and set to default if none, return false if invalid.
     emphasis = req.emphasis
     if emphasis == 0 or emphasis == None:
         emphasis == 0.5
     elif emphasis < 0.0 or emphasis > 1.0:
-        return NodResponse(False)
+        return PossibleResponse(False)
 
-    return BobbleResponse(True)
-
-
-#TODO Figure out what this motion should be.
-# A motion to catch attention 
-def handle_attention(req):
-
-    return AttentionResponse(True)
-
-
-# A fear-like motion, indicating danger in the area.
-def handle_danger(req):
-
-    return DangerResponse(True)nod = rospy.Service('/rcvm/nod', Nod, handle_nod)
-    print "Initialized nod server..."
-
+    return PossibleResponse(True)
 
 # An analog to the dive signal indicating an ascent by five meters.
 def handle_dive_ascend(req):
-
     return DiveAscendResponse(True)
-
 
 # An analog to the dive signal indicating a descent by five meters.
 def handle_dive_descend(req):
-
     return DiveDescendResponse(True)
 
-
 # An analog to the dive signal indicating that we should stay at the current depth for a bit.
-def handle_dive_stop(req):
+def handle_dive_wait(req):
+    return DiveWaitResponse(True)
 
-    return DiveStopResponse(True)
+# A motion to catch attention 
+def handle_attention(req):
+    return AttentionResponse(False)
 
+# A fear-like motion, indicating danger in the area.
+def handle_danger(req):
+    return DangerResponse(False)
 
 # A beckoning motion, indicating that the collabortor should follow.
 def handle_follow_me(req):
-
     return FollowMeResponse(True)
-
-
-# A confused motion, indicating the loss of localization.
-def handle_im_lost(req):
-
-    return ImLostResponse(True)    
-
 
 # A set of motions indicating vision, motor, or other failures.
 def handle_malfunction(req):
+    return MalfunctionResponse(False)
 
-    return MalfunctionResponse(True)
+# A "head cocking" motion asking for a repeat_last of the previous command.
+def handle_repeat_last(req):
+    return RepeatLastResponse(True)
 
+# A indidcate_objecting motion with the entire body, indicating the direction.
+def handle_indidcate_object(req):
+    return IndidcateObjectResponse(False)
 
-# A pointing motion with the entire body, indicating the direction.
-def handle_point(req):
-
-    return PointResponse(True)
-
-
-# A "head cocking" motion asking for a repeat of the previous command.
-def handle_repeat(req):
-
-    return RepeatResponse(True)
-
-
-# A motion indicating current battery state. Depending on battery state, 
-# could be a loop-de-loop or a limping motion.
+# A motion indicating current battery state.
 def handle_report_energy(req):
+    return HandleEnergyResponse(False)
 
-    return HandleEnergyResponse(True)
-
-# A motion indicating that the collabotor should remain in the current location.
-def handle_stay_here(req):
-
-    return StayHereResponse(True)
+# A confused motion, indicating the loss of localization.
+def handle_im_lost(req):
+    return ImLostResponse(False)
 
 
 # The centralized kineme server.
 def kineme_server():
-    rospy.init_node('rcvm_server')
+    rospy.init_affirmativee('rcvm_server')
 
     #Initialize all services.
 
-    nod = rospy.Service('/rcvm/nod', Nod, handle_nod)
-    print "Initialized nod server..."
+    affirmative = rospy.Service('/rcvm/affirmative', Affirmative, handle_affirmative)
+    print "Initialized Affirmative server..."
 
-    shake = rospy.Service('rcvm/shake', Shake, handle_shake)
-    print "Initialized shake server..."
+    negative = rospy.Service('rcvm/negative', Negative, handle_negative)
+    print "Initialized Negative server..."
 
-    bobble = rospy.Service('rcvm/bobble', Bobble, handle_bobble)
-    print "Initialized bobble server..."
-
-    attention = rospy.Service('/rcvm/attention', Attention, handle_attention)
-    print "Initialized attention server..."
-
-    danger = rospy.Service('/rcvm/danger', Danger, handle_danger)
-    print "Initialized danger server..."
+    possible = rospy.Service('rcvm/possible', Possible, handle_possible)
+    print "Initialized Possible server..."
 
     dive_ascend = rospy.Service('/rcvm/dive_ascend', DiveAscend, handle_dive_ascend)
-    print "Initialized dive ascend server..."
+    print "Initialized Dive Ascend server..."
 
     dive_descend = rospy.Service('/rcvm/dive_descend', DiveDescend, handle_dive_descend)
-    print "Initialized dive descend server..."
+    print "Initialized Dive Descend server..."
 
-    dive_stop = rospy.Service('/rcvm/dive_stop', DiveStop, handle_dive_stop)
-    print "Initialized dive stop server..."
+    dive_wait = rospy.Service('/rcvm/dive_wait', DiveWait, handle_dive_wait)
+    print "Initialized Dive Stop server..."
+
+    attention = rospy.Service('/rcvm/attention', Attention, handle_attention)
+    print "Initialized Attention server..."
+
+    danger = rospy.Service('/rcvm/danger', Danger, handle_danger)
+    print "Initialized Danger server..."
 
     follow_me = rospy.Service('/rcvm/follow_me', FollowMe, handle_follow_me)
-    print "Initialized follow me server..."
-
-    im_lost = rospy.Service('/rcvm/im_lost', ImLost, handle_im_lost)
-    print "Initialized i'm lost server..."
+    print "Initialized Follow Me server..."
 
     malfunction = rospy.Service('/rcvm/malfunction', Malfunction, handle_malfunction)
-    print "Initialized malfunction server..."
+    print "Initialized Malfunction server..."
 
-    point = rospy.Service('/rcvm/point', Point, handle_point)
-    print "Initialized point server..."
+    repeat_last = rospy.Service('/rcvm/repeat_last', RepeatLast, handle_repeat_last)
+    print "Initialized Repeat Last server..."
 
-    repeat = rospy.Service('/rcvm/repeat', Repeat, handle_repeat)
-    print "Initialized repeat server..."
+    indidcate_object = rospy.Service('/rcvm/indidcate_object', IndidcateObject, handle_indidcate_object)
+    print "Initialized Indidcate Object server..."
 
     report_energy = rospy.Service('/rcvm/report_energy', ReportEnergy, handle_report_energy)
-    print "Initialized report energy server..."
+    print "Initialized Report Energy server..."
 
-    stay_here = rospy.Service('/rcvm/stay_here', StayHere, handle_stay_here)
-    print "Initialized stay here server..."
+    im_lost = rospy.Service('/rcvm/im_lost', ImLost, handle_im_lost)
+    print "Initialized I'm Lost server..."
 
     rospy.spin()
 
